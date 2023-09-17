afa1559744317a385f48e955b0a5c61bb315e5e7
trivien11vn <triviencoderr@gmail.com>
2023-09-17 13:25:16 +0700

    add animation Roll

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float rollSpeed = 2f;
+    private float rollTime;
+    public float SetRollTime;
+    bool isRoll = false;
+
     private Rigidbody2D rb;
     public Vector3 moveInput;
     private Animator animator;
@@ -24,6 +29,20 @@ public class PlayerMove : MonoBehaviour
             else{
                 transform.localScale = new Vector3(-0.146989f,0.1711031f,0f);
             }
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && rollTime <= 0){
+            animator.SetBool("isRoll", true);
+            moveSpeed += rollSpeed;
+            rollTime = SetRollTime;
+            isRoll = true;
+        }
+        if(rollTime <= 0 && isRoll){
+            moveSpeed -= rollSpeed;
+            isRoll = false;
+            animator.SetBool("isRoll", false);
+        }
+        else{
+            rollTime -= Time.deltaTime;
         }
     }
 }
